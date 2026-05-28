3e8b3bce4b3b6e6aeb38e2a79af6bb75f4b72414
dbsehd125 <dbsehd125@gmail.com>
2026-05-28 17:41:17 +0900

    module: header, footer

---
+++ js/module/footer.js
@@ -0,0 +1,20 @@
+class Footer {
+  constructor() {
+    this.footer = `
+    <footer class="inner-pd d-flex flex-column align-items-center g-1">
+
+      <p>&copy; 2014. All rights reserved. Questions?</p>
+
+      <p>
+        Email us at
+        <a href="#" class="text-link">
+          office@product.com
+        </a>
+      </p>
+
+    </footer>
+    `;
+  }
+}
+
+export default Footer;
